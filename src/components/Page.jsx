7b0bee31025abff06f46c85b874789352aa6b1b3
pathan-hameed <abdulhameedkhan646.ahk@gmail.com>
2025-06-13 06:01:18 +0530

    structured the code

---
+++ src/components/Page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Page.css'
+
+export default function Page() {
+  return (
+    <section className="page-container">
+        this is page
+    </section>
+  )
+}
