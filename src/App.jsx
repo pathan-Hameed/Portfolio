2cccdcdbf80acf8fe5e19170ee62c9191157f867
pathan-hameed <abdulhameedkhan646.ahk@gmail.com>
2025-06-14 16:30:27 +0530

    plane canva

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import React from "react";
-import "./App.css";
-import { Route, Routes } from "react-router";
-import Home from "./pages/home";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-// import { Routes, Route } from "react-router-dom";
+import React from 'react'
 
 export default function App() {
   return (
-    <>
-    <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </>
-  );
+    <div>
+      <h1>hello meee</h1>
+    </div>
+  )
 }
